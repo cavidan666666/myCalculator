3df2161a0106aa194e229f59a8f7a761f9669b53
Cavidan <acavidan.p102@code.edu.az>
2017-09-05 00:33:56 +0400

    final commit

---
+++ clc/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace clc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button16 = new System.Windows.Forms.Button();
+            this.noqte = new System.Windows.Forms.Button();
+            this.duyme0 = new System.Windows.Forms.Button();
+            this.duyme7 = new System.Windows.Forms.Button();
+            this.duyme8 = new System.Windows.Forms.Button();
+            this.duyme9 = new System.Windows.Forms.Button();
+            this.duyme6 = new System.Windows.Forms.Button();
+            this.duyme5 = new System.Windows.Forms.Button();
+            this.topla = new System.Windows.Forms.Button();
+            this.cix = new System.Windows.Forms.Button();
+            this.vur = new System.Windows.Forms.Button();
+            this.bolme = new System.Windows.Forms.Button();
+            this.duyme4 = new System.Windows.Forms.Button();
+            this.duyme2 = new System.Windows.Forms.Button();
+            this.duyme3 = new System.Windows.Forms.Button();
+            this.duyme1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.temizle = new System.Windows.Forms.Button();
+            this.Sil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.Yellow;
+            this.button16.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button16.Location = new System.Drawing.Point(32, 270);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(122, 38);
+            this.button16.TabIndex = 33;
+            this.button16.Text = "=";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // noqte
+            // 
+            this.noqte.BackColor = System.Drawing.Color.Aqua;
+            this.noqte.Font = new System.Drawing.Font("Wide Latin", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noqte.Location = new System.Drawing.Point(170, 223);
+            this.noqte.Name = "noqte";
+            this.noqte.Size = new System.Drawing.Size(56, 38);
+            this.noqte.TabIndex = 32;
+            this.noqte.Text = ".";
+            this.noqte.UseVisualStyleBackColor = false;
+            this.noqte.Click += new System.EventHandler(this.noqte_Click);
+            // 
+            // duyme0
+            // 
+            this.duyme0.BackColor = System.Drawing.Color.Aqua;
+            this.duyme0.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme0.Location = new System.Drawing.Point(32, 224);
+            this.duyme0.Name = "duyme0";
+            this.duyme0.Size = new System.Drawing.Size(122, 38);
+            this.duyme0.TabIndex = 31;
+            this.duyme0.Text = "0";
+            this.duyme0.UseVisualStyleBackColor = false;
+            this.duyme0.Click += new System.EventHandler(this.duyme0_Click);
+            // 
+            // duyme7
+            // 
+            this.duyme7.BackColor = System.Drawing.Color.Aqua;
+            this.duyme7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme7.Location = new System.Drawing.Point(31, 171);
+            this.duyme7.Name = "duyme7";
+            this.duyme7.Size = new System.Drawing.Size(56, 38);
+            this.duyme7.TabIndex = 30;
+            this.duyme7.Text = "7";
+            this.duyme7.UseVisualStyleBackColor = false;
+            this.duyme7.Click += new System.EventHandler(this.duyme7_Click);
+            // 
+            // duyme8
+            // 
+            this.duyme8.BackColor = System.Drawing.Color.Aqua;
+            this.duyme8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme8.Location = new System.Drawing.Point(100, 171);
+            this.duyme8.Name = "duyme8";
+            this.duyme8.Size = new System.Drawing.Size(56, 38);
+            this.duyme8.TabIndex = 29;
+            this.duyme8.Text = "8";
+            this.duyme8.UseVisualStyleBackColor = false;
+            this.duyme8.Click += new System.EventHandler(this.duyme8_Click);
+            // 
+            // duyme9
+            // 
+            this.duyme9.BackColor = System.Drawing.Color.Aqua;
+            this.duyme9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme9.Location = new System.Drawing.Point(169, 171);
+            this.duyme9.Name = "duyme9";
+            this.duyme9.Size = new System.Drawing.Size(56, 38);
+            this.duyme9.TabIndex = 28;
+            this.duyme9.Text = "9";
+            this.duyme9.UseVisualStyleBackColor = false;
+            this.duyme9.Click += new System.EventHandler(this.duyme9_Click);
+            // 
+            // duyme6
+            // 
+            this.duyme6.BackColor = System.Drawing.Color.Aqua;
+            this.duyme6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme6.Location = new System.Drawing.Point(172, 118);
+            this.duyme6.Name = "duyme6";
+            this.duyme6.Size = new System.Drawing.Size(56, 38);
+            this.duyme6.TabIndex = 27;
+            this.duyme6.Text = "6";
+            this.duyme6.UseVisualStyleBackColor = false;
+            this.duyme6.Click += new System.EventHandler(this.duyme6_Click);
+            // 
+            // duyme5
+            // 
+            this.duyme5.BackColor = System.Drawing.Color.Aqua;
+            this.duyme5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme5.Location = new System.Drawing.Point(100, 118);
+            this.duyme5.Name = "duyme5";
+            this.duyme5.Size = new System.Drawing.Size(56, 38);
+            this.duyme5.TabIndex = 26;
+            this.duyme5.Text = "5";
+            this.duyme5.UseVisualStyleBackColor = false;
+            this.duyme5.Click += new System.EventHandler(this.duyme5_Click);
+            // 
+            // topla
+            // 
+            this.topla.BackColor = System.Drawing.Color.Yellow;
+            this.topla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topla.Location = new System.Drawing.Point(241, 69);
+            this.topla.Name = "topla";
+            this.topla.Size = new System.Drawing.Size(56, 38);
+            this.topla.TabIndex = 25;
+            this.topla.Text = "+";
+            this.topla.UseVisualStyleBackColor = false;
+            this.topla.Click += new System.EventHandler(this.topla_Click);
+            // 
+            // cix
+            // 
+            this.cix.BackColor = System.Drawing.Color.Yellow;
+            this.cix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cix.Location = new System.Drawing.Point(241, 224);
+            this.cix.Name = "cix";
+            this.cix.Size = new System.Drawing.Size(56, 38);
+            this.cix.TabIndex = 24;
+            this.cix.Text = "-";
+            this.cix.UseVisualStyleBackColor = false;
+            this.cix.Click += new System.EventHandler(this.cix_Click);
+            // 
+            // vur
+            // 
+            this.vur.BackColor = System.Drawing.Color.Yellow;
+            this.vur.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vur.Location = new System.Drawing.Point(241, 124);
+            this.vur.Name = "vur";
+            this.vur.Size = new System.Drawing.Size(56, 32);
+            this.vur.TabIndex = 23;
+            this.vur.Text = "*";
+            this.vur.UseVisualStyleBackColor = false;
+            this.vur.Click += new System.EventHandler(this.vur_Click);
+            // 
+            // bolme
+            // 
+            this.bolme.BackColor = System.Drawing.Color.Yellow;
+            this.bolme.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bolme.Location = new System.Drawing.Point(241, 171);
+            this.bolme.Name = "bolme";
+            this.bolme.Size = new System.Drawing.Size(56, 38);
+            this.bolme.TabIndex = 22;
+            this.bolme.Text = "/";
+            this.bolme.UseVisualStyleBackColor = false;
+            this.bolme.Click += new System.EventHandler(this.bolme_Click);
+            // 
+            // duyme4
+            // 
+            this.duyme4.BackColor = System.Drawing.Color.Aqua;
+            this.duyme4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme4.Location = new System.Drawing.Point(31, 118);
+            this.duyme4.Name = "duyme4";
+            this.duyme4.Size = new System.Drawing.Size(56, 38);
+            this.duyme4.TabIndex = 21;
+            this.duyme4.Text = "4";
+            this.duyme4.UseVisualStyleBackColor = false;
+            this.duyme4.Click += new System.EventHandler(this.duyme4_Click);
+            // 
+            // duyme2
+            // 
+            this.duyme2.BackColor = System.Drawing.Color.Aqua;
+            this.duyme2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme2.Location = new System.Drawing.Point(100, 69);
+            this.duyme2.Name = "duyme2";
+            this.duyme2.Size = new System.Drawing.Size(56, 38);
+            this.duyme2.TabIndex = 20;
+            this.duyme2.Text = "2";
+            this.duyme2.UseVisualStyleBackColor = false;
+            this.duyme2.Click += new System.EventHandler(this.duyme2_Click);
+            // 
+            // duyme3
+            // 
+            this.duyme3.BackColor = System.Drawing.Color.Aqua;
+            this.duyme3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme3.Location = new System.Drawing.Point(172, 69);
+            this.duyme3.Name = "duyme3";
+            this.duyme3.Size = new System.Drawing.Size(56, 38);
+            this.duyme3.TabIndex = 19;
+            this.duyme3.Text = "3";
+            this.duyme3.UseVisualStyleBackColor = false;
+            this.duyme3.Click += new System.EventHandler(this.duyme3_Click);
+            // 
+            // duyme1
+            // 
+            this.duyme1.BackColor = System.Drawing.Color.Aqua;
+            this.duyme1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duyme1.Location = new System.Drawing.Point(32, 69);
+            this.duyme1.Name = "duyme1";
+            this.duyme1.Size = new System.Drawing.Size(56, 38);
+            this.duyme1.TabIndex = 18;
+            this.duyme1.Text = "1";
+            this.duyme1.UseVisualStyleBackColor = false;
+            this.duyme1.Click += new System.EventHandler(this.duyme1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox1.Location = new System.Drawing.Point(32, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 59);
+            this.textBox1.TabIndex = 17;
+            // 
+            // temizle
+            // 
+            this.temizle.BackColor = System.Drawing.Color.Yellow;
+            this.temizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.temizle.Location = new System.Drawing.Point(241, 268);
+            this.temizle.Name = "temizle";
+            this.temizle.Size = new System.Drawing.Size(56, 38);
+            this.temizle.TabIndex = 34;
+            this.temizle.Text = "CE";
+            this.temizle.UseVisualStyleBackColor = false;
+            this.temizle.Click += new System.EventHandler(this.temizle_Click);
+            // 
+            // Sil
+            // 
+            this.Sil.BackColor = System.Drawing.Color.Yellow;
+            this.Sil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sil.Location = new System.Drawing.Point(169, 268);
+            this.Sil.Name = "Sil";
+            this.Sil.Size = new System.Drawing.Size(56, 38);
+            this.Sil.TabIndex = 35;
+            this.Sil.Text = "<====";
+            this.Sil.UseVisualStyleBackColor = false;
+            this.Sil.Click += new System.EventHandler(this.Sil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(329, 318);
+            this.Controls.Add(this.Sil);
+            this.Controls.Add(this.temizle);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.noqte);
+            this.Controls.Add(this.duyme0);
+            this.Controls.Add(this.duyme7);
+            this.Controls.Add(this.duyme8);
+            this.Controls.Add(this.duyme9);
+            this.Controls.Add(this.duyme6);
+            this.Controls.Add(this.duyme5);
+            this.Controls.Add(this.topla);
+            this.Controls.Add(this.cix);
+            this.Controls.Add(this.vur);
+            this.Controls.Add(this.bolme);
+            this.Controls.Add(this.duyme4);
+            this.Controls.Add(this.duyme2);
+            this.Controls.Add(this.duyme3);
+            this.Controls.Add(this.duyme1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button noqte;
+        private System.Windows.Forms.Button duyme0;
+        private System.Windows.Forms.Button duyme7;
+        private System.Windows.Forms.Button duyme8;
+        private System.Windows.Forms.Button duyme9;
+        private System.Windows.Forms.Button duyme6;
+        private System.Windows.Forms.Button duyme5;
+        private System.Windows.Forms.Button topla;
+        private System.Windows.Forms.Button cix;
+        private System.Windows.Forms.Button vur;
+        private System.Windows.Forms.Button bolme;
+        private System.Windows.Forms.Button duyme4;
+        private System.Windows.Forms.Button duyme2;
+        private System.Windows.Forms.Button duyme3;
+        private System.Windows.Forms.Button duyme1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button temizle;
+        private System.Windows.Forms.Button Sil;
+    }
+}
+
---
+++ clc/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clc
+{
+    public partial class Form1 : Form
+    {
+        int islem = 0;
+        double eded1, eded2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void duyme1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += "1";
+            
+        }
+
+        private void duyme2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += "2";
+            
+        }
+
+        private void duyme3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += "3";
+            
+        }
+
+        private void duyme4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += "4";
+            
+        }
+
+        private void duyme5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += "5";
+            
+        }
+
+        private void duyme6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += "6";
+            
+        }
+
+        private void duyme7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += "7";
+            
+        }
+
+        private void duyme8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += "8";
+            
+        }
+
+        private void duyme9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+           
+                textBox1.Text = "";
+                textBox1.Text += "9";
+            
+        }
+
+        private void duyme0_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += "0";
+            
+        }
+
+        private void topla_Click(object sender, EventArgs e)
+        {
+            islem = 1;
+            eded1 = Convert.ToDouble(textBox1.Text);
+            sifirla();
+      
+            
+        }
+        public void sifirla()
+        {
+            textBox1.Text = "0";
+        }
+       
+        public double hesabla()
+        {
+            double netice = 0;
+
+            if (islem == 1)
+            
+                netice = eded1 + eded2;
+                
+            
+            else if (islem == 2)
+            
+                netice = eded1 * eded2;
+            
+            else if(islem == 3)
+            
+                netice = eded1 / eded2;
+            
+            else if (islem == 4)
+            
+                netice = eded1 - eded2;
+           
+            else
+            
+                netice = 0;
+
+                return netice;
+            
+        }
+
+        private void temizle_Click(object sender, EventArgs e)
+        {
+            sifirla();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            eded2 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = hesabla().ToString();
+        }
+
+        private void vur_Click(object sender, EventArgs e)
+        {
+            islem = 2;
+            eded1 = Convert.ToDouble(textBox1.Text);
+            sifirla();
+        }
+
+        private void bolme_Click(object sender, EventArgs e)
+        {
+            islem = 3;
+            eded1 = Convert.ToDouble(textBox1.Text);
+            sifirla();
+        }
+
+        private void cix_Click(object sender, EventArgs e)
+        {
+            islem = 4;
+            eded1 = Convert.ToDouble(textBox1.Text);
+            sifirla();
+        }
+
+        private void Sil_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = "";
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                textBox1.Text += data[i].ToString();
+            }
+
+            if(textBox1.Text == "")
+            {
+                textBox1.Text = "0";
+            }
+        }
+
+        private void noqte_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            
+                textBox1.Text = "";
+                textBox1.Text += ".";
+            
+        }
+
+       
+    }
+}
